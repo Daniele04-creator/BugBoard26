83d438b24fb7eb1ef9fe041c1a08c4b464c24ebf
Daniele04-creator <daniele.deblasio2004@gmail.com>
2025-12-15 22:19:54 +0100

    hotspot sonar

--- backend/src/main/java/it/unina/bugboard26/backend/controller/SpaController.java
+++ backend/src/main/java/it/unina/bugboard26/backend/controller/SpaController.java
@@ -3,6 +3,7 @@ package it.unina.bugboard26.backend.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SpaController {
@@ -12,11 +13,14 @@ public class SpaController {
         return "forward:/index.html";
     }
 
-    @RequestMapping(value = {
-            "/",
-            "/{path:[^\\.]*}",
-            "/**/{path:[^\\.]*}"
-    })
+    @RequestMapping(
+            value = {
+                    "/",
+                    "/{path:[^\\.]*}",
+                    "/**/{path:[^\\.]*}"
+            },
+            method = {RequestMethod.GET, RequestMethod.HEAD}
+    )
     public String forward() {
         return "forward:/index.html";
     }
