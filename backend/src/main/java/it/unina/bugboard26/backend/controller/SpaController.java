99db91f10a15bb6c4f4e90e21b4d2554b9b4a972
Daniele04-creator <daniele.deblasio2004@gmail.com>
2025-12-15 14:44:05 +0100

    nuovo push

---
+++ backend/src/main/java/it/unina/bugboard26/backend/controller/SpaController.java
@@ -0,0 +1,17 @@
+package it.unina.bugboard26.backend.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+
+    @RequestMapping(value = {
+            "/",
+            "/{path:[^\\.]*}",
+            "/**/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
