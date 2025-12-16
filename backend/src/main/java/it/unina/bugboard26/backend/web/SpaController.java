eb366f2194df2d915b122466b110ea6042f91db9
Daniele04-creator <daniele.deblasio2004@gmail.com>
2025-12-16 11:14:20 +0100

    spa

--- backend/src/main/java/it/unina/bugboard26/backend/web/SpaController.java
+++ backend/src/main/java/it/unina/bugboard26/backend/web/SpaController.java
@@ -1,24 +1,14 @@
 package it.unina.bugboard26.backend.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SpaController {
 
-    @GetMapping("/Bugboard.web")
-    public String bugboardWeb() {
-        return "forward:/index.html";
-    }
-
     @RequestMapping(
-            value = {
-                    "/",
-                    "/{path:[^\\.]*}",
-                    "/**/{path:[^\\.]*}"
-            },
+            value = {"/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"},
             method = {RequestMethod.GET, RequestMethod.HEAD}
     )
     public String forward() {
